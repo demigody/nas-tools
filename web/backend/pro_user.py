2c99aaebc5b70c075d131dd7c870020fd0f370d1
demigody <yl1551234654@gmail.com>
2024-07-17 15:51:24 +0800

    fix(内置索引器): 修复索引器中没有mt

--- web/backend/pro_user.py
+++ web/backend/pro_user.py
@@ -308,7 +308,7 @@ class ProUser(UserMixin):
             recovery_msg = """
 ----------------------------------------------------------------------------------------------------------
 请检查是否曾经修改了 user.sites.bin 文件 (user.sites.bin 与官方或者其他版本不统一，不能相互覆盖)，请按照下面的方式尝试恢复:
-1. 如果曾经修改: 请去 https://raw.githubusercontent.com/demigody/nas-tools-sites/master/user.sites.bin 下载后还原
+1. 如果曾经修改: 请去 https://raw.githubusercontent.com/demigody/nas-tools/master/web/backend/user.sites.bin 下载后还原
 2. 如果从未修改: 请去 https://github.com/demigody/nas-tools/issues 创建issue，并附上报错信息
 ----------------------------------------------------------------------------------------------------------
             """
